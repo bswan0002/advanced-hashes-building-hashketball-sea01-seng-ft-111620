3d86ae54851980ce8c0961c00bc4cbb662fe86a2
Ben Swanson <bswan0002@gmail.com>
2020-10-15 16:52:15 +0000

    Automatically backed up by Learn

---
+++ hashketball.rb
@@ -0,0 +1,16 @@
+def game_hash
+  hashskethash = {
+    :home => {
+      :team_name => "stringOfHomeTeamName",
+      :colors => ["array", "of", "homeTeam", "colors", "as", "strings"],
+      :players => [
+        "Alan Anderson" => {
+          
+        }
+      ]
+    }
+    :away => {
+      
+    }
+  }
+end
