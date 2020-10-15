3eccd1dde48bcd331c6b51885f685d1bd4af90de
Ben Swanson <bswan0002@gmail.com>
2020-10-15 16:58:51 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,63 @@
 def game_hash
   hashskethash = {
     :home => {
-      :team_name => "stringOfHomeTeamName",
-      :colors => ["array", "of", "homeTeam", "colors", "as", "strings"],
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black", "White"],
       :players => [
-        "Alan Anderson" => {
-          
+        {
+          :player_name => "Alan Anderson",
+          :number => "0",
+          :shoe => "16",
+          :points => "22",
+          :rebounds => ,
+          :assists => ,
+          :steals => ,
+          :blocks => ,
+          :slam_dunks =>
+        },
+        {
+          :player_name => "Alan Anderson",
+          :number => "0",
+          :shoe => "16",
+          :points => "22",
+          :rebounds => ,
+          :assists => ,
+          :steals => ,
+          :blocks => ,
+          :slam_dunks =>
+        },
+        {
+          :player_name => "Alan Anderson",
+          :number => "0",
+          :shoe => "16",
+          :points => "22",
+          :rebounds => ,
+          :assists => ,
+          :steals => ,
+          :blocks => ,
+          :slam_dunks =>
+        },
+        {
+          :player_name => "Alan Anderson",
+          :number => "0",
+          :shoe => "16",
+          :points => "22",
+          :rebounds => ,
+          :assists => ,
+          :steals => ,
+          :blocks => ,
+          :slam_dunks =>
+        },
+        {
+          :player_name => "Alan Anderson",
+          :number => "0",
+          :shoe => "16",
+          :points => "22",
+          :rebounds => ,
+          :assists => ,
+          :steals => ,
+          :blocks => ,
+          :slam_dunks =>
         }
       ]
     }
